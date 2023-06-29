81b8b3124f6aeec2c58f937899189dd373d6aee6
dmitry2d <volcharo@gmail.com>
2023-06-29 15:05:38 +0300

    code

---
+++ banner.php
@@ -0,0 +1,20 @@
+<!-- Banner -->
+<div class="banner">
+    <div class="wrapper">
+        <div class="container">
+            <div class="title">Мы разрабатывем</div>
+            <div class="subtitle">удобные цифровые сервисы и медиаконтент</div>
+            <div class="description">Помогаем нашим клиентам в достижении успеха</div>
+            <div class="socials">
+                <a href="" class="call"></a>
+                <a href="" class="wa"></a>
+                <a href="" class="tg"></a>
+                <a href="" class="chat"></a>
+            </div>
+        </div>
+        <div class="thing thing1"><img src="./assets/img/banner_thing_1.svg" alt=""></div>
+        <div class="thing thing2"><img src="./assets/img/banner_thing_2.svg" alt=""></div>
+        <div class="thing thing3"><img src="./assets/img/banner_thing_3.png" alt=""></div>
+    </div>
+</div>
+<!-- / Banner -->
---
+++ header.php
@@ -0,0 +1,27 @@
+<!-- Header -->
+<div class="header">
+    <div class="wrapper">
+        <div class="container">
+            <div class="logo">
+                <a href="">
+                    <img src="./assets/img/logo.png" alt="">
+                </a>
+            </div>
+            <div class="phone">
+                <a href="">
+                    <div class="img"></div>
+                    <div class="text">
+                        8 800 600 83 17
+                    </div>
+                </a>
+            </div>
+            <div class="cabinet">
+                <a href=""></a>
+            </div>
+            <div class="hamb">
+                <a href=""></a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- / Header -->
